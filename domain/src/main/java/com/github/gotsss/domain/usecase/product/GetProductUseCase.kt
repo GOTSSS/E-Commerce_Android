7e51b66e44cd33e804b1553dbfcf0f8a1ee8979f
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-07-17 17:04:21 +0200

    Update GetProductUseCase.kt

--- domain/src/main/java/com/github/gotsss/domain/usecase/product/GetProductUseCase.kt
+++ domain/src/main/java/com/github/gotsss/domain/usecase/product/GetProductUseCase.kt
@@ -1,15 +1,17 @@
 package com.github.gotsss.domain.usecase.product
 
-import com.github.gotsss.domain.entity.AppResult
+import com.github.gotsss.domain.base.AppResult
+import com.github.gotsss.domain.base.FlowUseCase
 import com.github.gotsss.domain.entity.product.ProductEntity
 import com.github.gotsss.domain.repository.ProductRepository
-import com.github.gotsss.domain.usecase.BaseUseCase
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetProductUseCase @Inject constructor(
     private val productRepository: ProductRepository
-): BaseUseCase<Long, ProductEntity>() {
-
-    override suspend fun execute(parameters: Long): AppResult<ProductEntity> =
-        productRepository.getProduct(parameters)
+): FlowUseCase<Long, ProductEntity>() {
+    override suspend fun execute(
+        parameters: Long,
+        refresh: Boolean
+    ): Flow<AppResult<ProductEntity>> = productRepository.getProduct(parameters)
 }
