84b4a05e107b75ab370c2bc63fd567e0cd8eb2fe
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2023-07-17 17:04:26 +0200

    Update ProductRepository.kt

--- domain/src/main/java/com/github/gotsss/domain/repository/ProductRepository.kt
+++ domain/src/main/java/com/github/gotsss/domain/repository/ProductRepository.kt
@@ -1,14 +1,15 @@
 package com.github.gotsss.domain.repository
 
-import com.github.gotsss.domain.entity.AppResult
+import com.github.gotsss.domain.base.AppResult
 import com.github.gotsss.domain.entity.FilterEntity
 import com.github.gotsss.domain.entity.product.ProductEntity
 import com.github.gotsss.domain.entity.product.ProductItemEntity
+import kotlinx.coroutines.flow.Flow
 
 interface ProductRepository {
 
-    suspend fun getProducts(filter: FilterEntity): AppResult<List<ProductItemEntity>?>
+    suspend fun getProducts(filter: FilterEntity): Flow<AppResult<List<ProductItemEntity>?>>
 
-    suspend fun getProduct(id: Long): AppResult<ProductEntity>
+    suspend fun getProduct(id: Long): Flow<AppResult<ProductEntity>>
 
 }
